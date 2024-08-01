b3d954ac0528d41cdcd4fd920bce7d25fb8cfb52
pavankalyan2809 <pavankalyanbhuthagaddala@gmail.com>
2024-08-01 07:04:30 +0000

    Simple Todos

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -1,3 +1,9 @@
+import {Component} from 'react'
+
+import TodoItem from '../TodoItem'
+
+import './index.css'
+
 const initialTodosList = [
   {
     id: 1,
@@ -33,4 +39,40 @@ const initialTodosList = [
   },
 ]
 
-// Write your code here
+class SimpleTodos extends Component {
+  state = {
+    todosList: initialTodosList,
+  }
+
+  deleteTodo = id => {
+    const {todosList} = this.state
+    const updatedTodosList = todosList.filter(eachTodo => eachTodo.id !== id)
+
+    this.setState({
+      todosList: updatedTodosList,
+    })
+  }
+
+  render() {
+    const {todosList} = this.state
+
+    return (
+      <div className="app-container">
+        <div className="simple-todos-container">
+          <h1 className="heading">Simple Todos</h1>
+          <ul className="todos-list">
+            {todosList.map(eachTodo => (
+              <TodoItem
+                key={eachTodo.id}
+                todoDetails={eachTodo}
+                deleteTodo={this.deleteTodo}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SimpleTodos
